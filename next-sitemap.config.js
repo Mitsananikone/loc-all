8c87c6f62246c7012a59c0650cc4f441a16b99a6
Mit Sananikone <Mitsananikone@gmail.com>
2025-04-21 18:59:02 -0500

    next-sitemap.config.js now excludes pages

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     siteUrl: 'https://loc-all.vercel.app',
     generateRobotsTxt: true, // Optional
-    exclude: ['/admin', '/private'], // Pages to exclude
+    exclude: [
+       // 🚫 Exclude duplicate or unwanted paths
+    '/donations/donations', 
+    '/about/about',
+    '/location/location',
+    '/mission/mission',
+    '/server-sitemap.xml',  // Default exclusion for API routes
+    '/404',                 // Exclude error pages
+    '/admin',               // Exclude admin panels
+    '/private/*',           // Exclude all private routes
+    ], // Pages to exclude
   };
